fe03663d1e0c532a7c276a36f5dd80d71e15acb1
Mohamed Hatem <68520226+mohatem99@users.noreply.github.com>
2024-08-08 12:22:34 +0300

    Update connectionDb.js

--- db/connectionDb.js
+++ db/connectionDb.js
@@ -4,6 +4,8 @@ const connectionDb = async () => {
   try {
     await mongoose.connect(process.env.DB_URI_ONLLINE);
     console.log("Database connected");
+
+    console.log("process.env.DB_URI_ONLLINE");
   } catch (err) {
     console.log("Error connecting to Database", err);
   }
