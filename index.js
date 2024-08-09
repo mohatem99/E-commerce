cfc890425d7cce3c821981535c2da84a6283e968
Mohamed Hatem <68520226+mohatem99@users.noreply.github.com>
2024-08-09 19:15:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
   if (req.originalUrl == "/orders/webhook") {
+    express.raw({ type: "application/json" });
     next();
   } else {
     express.json()(req, res, next);
